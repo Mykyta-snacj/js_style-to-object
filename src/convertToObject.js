85bfe277d3d6045370d89969f77e638b09a12f83
Mykyta Kalniev <nikitamal4ik@gmail.com>
2024-12-19 12:40:26 +0100

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesList = sourceString.replaceAll('\n', ' ').split(';');
+  const stylesObject = {};
+  const filtredStylesList = stylesList.filter((val) => val.trim().length > 0);
+
+  for (let i = 0; i < filtredStylesList.length; i++) {
+    filtredStylesList[i] = filtredStylesList[i].split(':');
+    filtredStylesList[i][0] = filtredStylesList[i][0].trim();
+    filtredStylesList[i][1] = filtredStylesList[i][1].trim();
+    filtredStylesList[i][1] = filtredStylesList[i][1].replaceAll(', ', ',\n');
+    stylesObject[filtredStylesList[i][0]] = filtredStylesList[i][1];
+  }
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;
